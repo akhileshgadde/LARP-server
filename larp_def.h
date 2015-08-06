12b3009c94b2429fc420b9ce779ca9ffad8c7f67
akhileshgadde <akhilesh.gadde@outlook.com>
2015-08-05 18:09:15 -0700

    updated makefile and lib folder, 5th Aug

--- larp_def.h
+++
@@ -1,3 +0,0 @@
-#define ARPHRD_SLIP     256
-#define ARPHRD_LARP ARPHRD_SLIP         /*temporary assignment for LARP */
-#include <linux/if_ether.h>
--- thread_mutex.h
+++
@@ -1,2 +0,0 @@
-/* variable for pthread mutexes */
-pthread_mutex_t mutex_lock = PTHREAD_MUTEX_INITIALIZER;
